b5178ef4ced20e441e1fd823198acd2186787327
숙제 <tmdgus507@naver.com>
2017-06-13 18:57:21 +0900

    pretty

--- tetris_test.py
+++ tetris_test.py
@@ -7,13 +7,15 @@ from pygame.locals import *
 # pretty UI
 class P_UI:
     """docstring for P_UI"""
-    # Background
+    # Tetris_Background
     black = (10, 10, 10)
     black_2 = (26, 26, 26)
     white = (255, 255, 255)
+    grey = (35, 35, 35)
+    grey_2 = (55, 55, 55)
 
-    background = black
-
+    t_background = black_2
+    backcolor = white
     # Piece_color
     cyan = (69, 206, 204)
     blue = (64, 111, 249)
@@ -24,7 +26,13 @@ class P_UI:
     red = (225, 13, 27)
 
     # PIECES = {1: O, 2: I, 3: L, 4: J, 4ㅣ5: Z, 6:S, 7:T}
-    COLORS = {1: yellow, 2: cyan, 3: orange, 4: blue, 5: red, 6: green, 7:pink}
+    COLORS = { 1: yellow, 2: cyan, 3: orange, 4: blue, 5: red, 6: green, 7:pink}
+
+    # width
+    WIDTH = 500
+    # height
+    HEIGHT = 520
+
 
 class Mino:
 
@@ -32,7 +40,7 @@ class Mino:
         if piece_name:
             self.piece_name = piece_name
         else:
-            self.piece_name = random.choice(range(1, 8))
+            self.piece_name = random.randrange(1, 8)
         self.rotation = 0
         self.array2d = Piece.PIECES[self.piece_name][self.rotation]
         self.color = P_UI.COLORS[self.piece_name]
@@ -47,24 +55,27 @@ class Mino:
         self.array2d = Piece.PIECES[self.piece_name][self.rotation]
 
 class Board:
-    COLLIDE_ERROR = {'no_error': 0, 'right_wall': 1, 'left_wall': 2,
+    COLLIDE_EVENT = {'no_error': 0, 'right_wall': 1, 'left_wall': 2,
                      'bottom': 3, 'overlap': 4}
 
     def __init__(self, screen):
         self.screen = screen
-        self.width = 10
-        self.height = 22
-        self.block_size = 25
+        self.t_width = 10
+        self.t_height = 25
+        self.block_size = 20
         self.board = []
-        for _ in range(self.height):
-            self.board.append([0] * self.width)
+        for _ in range(self.t_height):
+            self.board.append([0] * self.t_width)
         self.generate_piece()
 
     def generate_piece(self):
         mino = Mino()
         self.piece = mino
         self.color = mino.color
-        self.piece_x, self.piece_y = 3, 0
+        self.piece_x, self.piece_y = 3, 2
+
+    def pos_to_pixel(self, x, y):
+        return self.block_size*(x+2), self.block_size*(y-2)
 
     def absorb_piece(self):
         for y, row in enumerate(self.piece):
@@ -75,14 +86,14 @@ class Board:
 
     def _block_collide_with_board(self, x, y):
         if x < 0:
-            return Board.COLLIDE_ERROR['left_wall']
-        elif x >= self.width:
-            return Board.COLLIDE_ERROR['right_wall']
-        elif y >= self.height:
-            return Board.COLLIDE_ERROR['bottom']
+            return Board.COLLIDE_EVENT['left_wall']
+        elif x >= self.t_width:
+            return Board.COLLIDE_EVENT['right_wall']
+        elif y >= self.t_height:
+            return Board.COLLIDE_EVENT['bottom']
         elif self.board[y][x]:
-            return Board.COLLIDE_ERROR['overlap']
-        return Board.COLLIDE_ERROR['no_error']
+            return Board.COLLIDE_EVENT['overlap']
+        return Board.COLLIDE_EVENT['no_error']
 
     def collide_with_board(self, dx, dy):
         """Check if piece (offset dx, dy) collides with board"""
@@ -92,7 +103,7 @@ class Board:
                     collide = self._block_collide_with_board(x=x+dx, y=y+dy)
                     if collide:
                         return collide
-        return Board.COLLIDE_ERROR['no_error']
+        return Board.COLLIDE_EVENT['no_error']
 
     def _can_move_piece(self, dx, dy):
         dx_ = self.piece_x + dx
@@ -109,14 +120,14 @@ class Board:
         collide = self.collide_with_board(dx=self.piece_x, dy=self.piece_y)
         if not collide:
             pass
-        elif collide == Board.COLLIDE_ERROR['left_wall']:
+        elif collide == Board.COLLIDE_EVENT['left_wall']:
             if self._can_move_piece(dx=1, dy=0):
                 self.move_piece(dx=1, dy=0)
             elif self._can_move_piece(dx=2, dy=0):
                 self.move_piece(dx=2, dy=0)
             else:
                 self.piece.rotate(not clockwise)
-        elif collide == Board.COLLIDE_ERROR['right_wall']:
+        elif collide == Board.COLLIDE_EVENT['right_wall']:
             if self._can_move_piece(dx=-1, dy=0):
                 self.move_piece(dx=-1, dy=0)
             elif self._can_move_piece(dx=-2, dy=0):
@@ -146,9 +157,6 @@ class Board:
     def rotate_piece(self, clockwise=True):
         self._try_rotate_piece(clockwise)
 
-    def pos_to_pixel(self, x, y):
-        return self.block_size*x, self.block_size*(y-2)
-
     def _delete_line(self, y):
         for y in reversed(range(1, y+1)):
             self.board[y] = list(self.board[y-1])
@@ -161,36 +169,62 @@ class Board:
     def game_over(self):
         return sum(self.board[0]) > 0 or sum(self.board[1]) > 0
 
-    def draw_blocks(self, array2d, dx=0, dy=0):
+    def draw_blocks(self, array2d, dx=0, dy=0, board=0):
         for y, row in enumerate(array2d):
             y += dy
-            if y >= 2 and y < self.height:
+            if y >= 2 and y < self.t_height:
                 for x, block in enumerate(row):
+                    x += dx
+                    x_pix, y_pix = self.pos_to_pixel(x, y)
                     if block:
-                        x += dx
-                        x_pix, y_pix = self.pos_to_pixel(x, y)
-# match block and color
-# color = P_UI.COLORS [block]
+                        # match block and color
+                        color = P_UI.COLORS [block]
                         # draw block
                         pygame.draw.rect(self.screen, color,
                                          (  x_pix, y_pix,
                                             self.block_size,
                                             self.block_size))
-                        # draw border
-                        pygame.draw.rect(self.screen, P_UI.background,
-                                         (  x_pix, y_pix,
-                                            self.block_size,
-                                            self.block_size), 1)
+                    if board:
+                        pygame.draw.rect(self.screen, P_UI.grey,
+                        (  x_pix, y_pix,
+                            self.block_size,
+                            self.block_size), 1)
+                        
+    def draw_board(self):
+        x_pix, y_pix = self.pos_to_pixel(0, 1)
+        self.screen.fill(P_UI.backcolor)
+
+        # pygame.draw.rect(self.screen,
+        #             P_UI.black,
+        #             Rect(x_pix-20, y_pix-20, 
+        #             self.t_width * self.block_size +40, (self.t_height-1) * self.block_size +40))
+        # pygame.draw.rect(self.screen,
+        #             P_UI.green,
+        #             Rect(x_pix-20, y_pix-20, 
+        #             self.t_width * self.block_size +40, (self.t_height-1) * self.block_size +40),3)        
+        
+        pygame.draw.rect(self.screen,
+                    P_UI.grey_2,
+                    Rect(x_pix, y_pix, 
+                    self.t_width * self.block_size, (self.t_height-1) * self.block_size),13)        
+
+        pygame.draw.rect(self.screen,
+                    P_UI.t_background,
+                    Rect(x_pix, y_pix, 
+                    self.t_width * self.block_size, (self.t_height-1) * self.block_size))
+   
+
 
     def draw(self):
+        self.draw_board()
         self.draw_blocks(self.piece, dx=self.piece_x, dy=self.piece_y)
-        self.draw_blocks(self.board)
+        self.draw_blocks(self.board, board=1)
 
 class Tetris:
     DROP_EVENT = USEREVENT + 1
 
     def __init__(self):
-        self.screen = pygame.display.set_mode((250, 500))
+        self.screen = pygame.display.set_mode((P_UI.WIDTH, P_UI.HEIGHT))
         self.clock = pygame.time.Clock()
         self.board = Board(self.screen)
 
@@ -224,7 +258,6 @@ class Tetris:
                 print ("Game over")
                 pygame.quit()
                 sys.exit()
-            self.screen.fill(P_UI.background)
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
